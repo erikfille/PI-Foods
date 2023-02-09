0a431e1f12e84f13b54290c13ebc7db215d2324d
erikfille <erikfilleaudeau@gmail.com>
2023-02-09 11:03:15 -0300

    test typo change

--- api/tests/routes/country.spec.js
+++ api/tests/routes/country.spec.js
@@ -6,7 +6,7 @@ const { Recipe, conn } = require('../../src/db.js');
 
 const agent = session(app);
 const recipe = {
-  name: 'Milanea a la napolitana',
+  name: 'Milanesa a la napolitana',
 };
 
 describe('Recipe routes', () => {
